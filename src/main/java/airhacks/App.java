f145ec953a5f736b0d05c9ebb3633798258450bc
Adam Bien <abien@adam-bien.com>
2023-10-01 16:15:27 +0200

    app renamed

--- src/main/java/airhacks/App.java
+++ src/main/java/airhacks/App.java
@@ -1,24 +1,15 @@
 package airhacks;
 
-import java.util.Arrays;
-
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.services.ec2.model.Tag;
 import software.amazon.awssdk.services.sts.StsClient;
 
 /**
  *
  * @author airhacks.com
  */
-public class SDKApp {
+interface App {
 
-    public static void main(String[] args) {
-        var appName = "aws-sdk-plain";
-        var project = Tag.builder().key("project").value("airhacks").build();
-        var environment = Tag.builder().key("environment").value("test").build();
-        var application = Tag.builder().key("application").value(appName).build();
-        var tags = Arrays.asList(project, environment, application);
-
+    static void main(String... args) {
         //current accessKey
         var credentialsProvider = DefaultCredentialsProvider.builder().build();
         var accessKey = credentialsProvider.resolveCredentials().accessKeyId();
